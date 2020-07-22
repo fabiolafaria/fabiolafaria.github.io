6dcaff62b966e5aca91fb7477a65397e5b0c50ec
Fabiola Faria <fabiola.analytics@gmail.com>
2020-07-22 22:11:40 +0000

    Updated '_includes/googleanalytics.js' via CloudCannon

--- _includes/googleanalytics.js
+++ _includes/googleanalytics.js
@@ -1,10 +1,8 @@
- <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-147256894-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
+window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-147256894-1');
-</script>
+
+
 
