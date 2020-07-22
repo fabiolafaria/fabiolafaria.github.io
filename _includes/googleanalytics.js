8a7a89da9fe511865fb7f0b7382aeacafc0d302e
Fabiola Faria <fabiola.analytics@gmail.com>
2020-07-22 22:21:04 +0000

    Updated '_includes/googleanalytics.js' via CloudCannon

--- _includes/googleanalytics.js
+++ _includes/googleanalytics.js
@@ -3,7 +3,7 @@ function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-147256894-1', 'auto');
-  ga('send', 'pageview');
+    gtag('js', new Date());
+    gtag('config', 'UA-147256894-1');
 
 
