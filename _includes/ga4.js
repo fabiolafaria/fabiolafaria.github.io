7b796f84c309095812ce2b304c6442c798724ef0
Fabiola <fabiola.analytics@gmail.com>
2023-05-21 18:00:34 +0100

    ga js

--- _includes/ga4.js
+++ _includes/ga4.js
@@ -1,5 +1,16 @@
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+function loadScriptAsync(scriptSrc, callback) {
+    if (typeof callback !== 'function') {
+        throw new Error('Not a valid callback for async script load');
+    }
+    var script = document.createElement('script');
+    script.onload = callback;
+    script.src = scriptSrc;
+    document.head.appendChild(script);
+}
 
-  gtag('config', 'G-QYR4Q2TDXJ');
+loadScriptAsync('https://www.googletagmanager.com/gtag/js?id=G-QYR4Q2TDXJ', function () {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-QYR4Q2TDXJ', { 'anonymize_ip': true });
+})
